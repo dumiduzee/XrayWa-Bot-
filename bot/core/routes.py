85560dd2c3757a7316729cef6a823651cdbe591f
dumiduzee <dumidulk@outlook.com>
2025-09-21 11:24:47 +0530

    change typo in stages

--- bot/core/routes.py
+++ bot/core/routes.py
@@ -29,16 +29,11 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
 
     NUMBER = payload.data.messages.key.remoteJid.split("@s.whatsapp.net")[0]
    
-
-
     user = user_status(phone_number=NUMBER,db=db)
     if user is False:
         #send respond to the the he is banned
         send_message(NUMBER,messages["BANNED_USER"])
          #continue with the process
-
-
-    
 
 
     if MESSAGE == "start" or stages["START"] == Redis.cache_getter(key=NUMBER):
@@ -55,7 +50,7 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                 hasConfig = CheckUserHaveConfig(number=NUMBER,db=db)
                 if hasConfig:
                     send_message(NUMBER,content="*You already have a config! you cannot create anymore!! 😤*")
-                    Redis.cache_setter(f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                     return
                 else:
                     send_message(NUMBER,messages["MAIN_MENU_01_MESSAGE"])
@@ -67,7 +62,7 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                 configs = get_configs(phone=NUMBER,db=db)
                 if configs.data[0]["config"] is None:
                     send_message(NUMBER,messages["MAIN_MENU_02_MESSAGE_WHEN_NO_CONFIGS"])
-                    Redis.cache_setter(f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                 else:
                     #get the config and pass it to the user
                     send_message(NUMBER,
@@ -78,7 +73,7 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                                 
                     *`{configs.data[0]["config"]}`*
                     """)
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case "3":
                 pass
 
@@ -87,7 +82,7 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
             case "5":
                 #return all the package details and prices
                 send_message(NUMBER,messages["MAIN_MENU_05_MESSAGE"])
-                Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
 
 
     elif(user_stage == stages["MAIN_MENU_01_STAGE"]):
@@ -100,10 +95,10 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                     result = SaveConfig(config=config,username=username,number=NUMBER,db=db)
                     if result:
                         config_Created_message(NUMBER,config,username)
-                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                         return
                     send_message(number=NUMBER,content="*Something went wrong on our side!😳*")
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case "2":
                 #create config via marzban
                 config,username = marzban_config_create(package=PACKAGES["MOBITEL"],username=f"{NUMBER}_{str(uuid.uuid4()).split("-")[2]}")
@@ -112,10 +107,10 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                     result = SaveConfig(config=config,username=username,number=NUMBER,db=db)
                     if result:
                         config_Created_message(NUMBER,config,username)
-                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                         return
                     send_message(number=NUMBER,content="*Something went wrong on our side!😳*")
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case "3":
                 #create config via marzban
                 config,username = marzban_config_create(package=PACKAGES["AIRTEL"],username=f"{NUMBER}_{str(uuid.uuid4()).split("-")[2]}")
@@ -124,10 +119,10 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                     result = SaveConfig(config=config,username=username,number=NUMBER,db=db)
                     if result:
                         config_Created_message(NUMBER,config,username)
-                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                         return
                     send_message(number=NUMBER,content="*Something went wrong on our side!😳*")
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case "4":
                 #create config via marzban
                 config,username = marzban_config_create(package=PACKAGES["HUTCH"],username=f"{NUMBER}_{str(uuid.uuid4()).split("-")[2]}")
@@ -136,10 +131,10 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                     result = SaveConfig(config=config,username=username,number=NUMBER,db=db)
                     if result:
                         config_Created_message(NUMBER,config,username)
-                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                         return
                     send_message(number=NUMBER,content="*Something went wrong on our side!😳*")
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case "5":
                 #create config via marzban
                 config,username = marzban_config_create(package=PACKAGES["SLT-ZOOM"],username=f"{NUMBER}_{str(uuid.uuid4()).split("-")[2]}")
@@ -148,10 +143,10 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                     result = SaveConfig(config=config,username=username,number=NUMBER,db=db)
                     if result:
                         config_Created_message(NUMBER,config,username)
-                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                         return
                     send_message(number=NUMBER,content="*Something went wrong on our side!😳*")
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case "6":
                 #create config via marzban
                 config,username = marzban_config_create(package=PACKAGES["SLT-NETFLIX"],username=f"{NUMBER}_{str(uuid.uuid4()).split("-")[2]}")
@@ -160,13 +155,13 @@ def webhook_handler(payload:WhatsAppEvent,db=Depends(getClient)):
                     result = SaveConfig(config=config,username=username,number=NUMBER,db=db)
                     if result:
                         config_Created_message(NUMBER,config,username)
-                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                        Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
                         return
                     send_message(number=NUMBER,content="*Something went wrong on our side!😳*")
-                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                    Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             case _ :
                 send_message(NUMBER,content="*Invalid choice please choose number between 1 to 6!🖖*")
-                Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["INITIAL_START"])
+                Redis.cache_setter(key=f"stage_{NUMBER}",ex=env.REDIS_EXPIRE_TIME,value=stages["START"])
             
 
                     
